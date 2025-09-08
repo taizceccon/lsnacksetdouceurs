358705c77e7fe4e4c3714381a5d0dd4bef122d34
taizceccon <taizceccon@gmail.com>
2025-09-08 17:20:54 +0200

    Ajout d'un test PHPUnit  pour CI

--- app/tests/bootstrap.php
+++ app/tests/bootstrap.php
@@ -2,7 +2,7 @@
 
 use Symfony\Component\Dotenv\Dotenv;
 
-require dirname(__DIR__).'/vendor/autoload.php';
+require __DIR__ . '/../../vendor/autoload.php';
 
 if (method_exists(Dotenv::class, 'bootEnv')) {
     (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
