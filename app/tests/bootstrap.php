5e5213501f2b0e322de7bfff8c74bcdf3858045f
taizceccon <taizceccon@gmail.com>
2025-09-08 18:15:46 +0200

    Ajout workflow CI pour ProductTest

--- app/tests/bootstrap.php
+++ app/tests/bootstrap.php
@@ -2,12 +2,11 @@
 
 use Symfony\Component\Dotenv\Dotenv;
 
-// Le vendor est à la racine du projet, donc on remonte d'un niveau depuis app/
-require dirname(__DIR__) . '/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 if (method_exists(Dotenv::class, 'bootEnv')) {
-    // Charger le .env.test pour CI
-    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env.test');
+    // Charge le .env.test pour la CI
+    (new Dotenv())->bootEnv(__DIR__ . '/../.env.test');
 }
 
 if ($_SERVER['APP_DEBUG'] ?? false) {
